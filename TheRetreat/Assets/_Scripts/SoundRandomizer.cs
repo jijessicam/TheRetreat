bf4404d064e921d29ca0df991f404dda222e6dfa
Luke Petruzzi <lukepetruzzi@gmail.com>
2018-04-10 01:49:01 -0400

    updated sound randomizer and added random noises and fixed crowd language

--- TheRetreat/Assets/_Scripts/SoundRandomizer.cs
+++ TheRetreat/Assets/_Scripts/SoundRandomizer.cs
@@ -8,20 +8,19 @@ public class SoundRandomizer : MonoBehaviour {
 	public string soundsFolderName;
 	public float minTimeBetweenSounds;
 	public float maxTimeBetweenSounds;
+	public bool randomizePanning;
 
-	public float timePassed = 0f;
-	public float nextSoundTime;
-	public Object[] soundsFolder;
-	public float timeToWaitForSoundToFinish = 0f;
+	float timePassed = 0f;
+	float nextSoundTime;
+	Object[] soundsFolder;
+	float timeToWaitForSoundToFinish = 0f;
 
 	// Use this for initialization
 	void Start () {
-
 		soundsFolder = Resources.LoadAll (soundsFolderName);
 
 		timePassed = 0f;
 		nextSoundTime = Random.Range (minTimeBetweenSounds, maxTimeBetweenSounds);
-		
 	}
 	
 	// Update is called once per frame
@@ -33,6 +32,11 @@ public class SoundRandomizer : MonoBehaviour {
 		if (timePassed > nextSoundTime && timeToWaitForSoundToFinish < 0f) {
 			audioSource.clip = (AudioClip) soundsFolder[Mathf.RoundToInt(Random.Range(0f, (float)soundsFolder.Length) - 1f)] as AudioClip;
 			timeToWaitForSoundToFinish = audioSource.clip.length;
+
+			if (randomizePanning) {
+				audioSource.panStereo = Random.Range (-1f, 1f);
+			}
+
 			audioSource.Play ();
 
 			timePassed = 0f;
