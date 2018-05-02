1174f023e17fa6fa19308803fd22848875bb275e
Luke Petruzzi <lukepetruzzi@gmail.com>
2018-05-01 23:20:16 -0400

    marker handler works

--- TheRetreat/Assets/_Scripts/CompassQuestMarkerHandler.cs
+++ TheRetreat/Assets/_Scripts/CompassQuestMarkerHandler.cs
@@ -1,26 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using PixelCrushers.DialogueSystem;
+using UnityEngine;
 
 public class CompassQuestMarkerHandler : MonoBehaviour {
 
 	public GameObject questMarkerPrefab;
 
+	private Hashtable activeMarkers;
+
 	// Use this for initialization
 	void Start () {
-		QuestLog.AddQuestStateObserver("Find Your Tent", LuaWatchFrequency.EveryDialogueEntry, OnQuestStateChanged);
+		QuestLog.AddQuestStateObserver ("Find Your Tent", LuaWatchFrequency.EveryUpdate, OnQuestStateChanged);
+		activeMarkers = new Hashtable();
 	}
-	
-	void OnQuestStateChanged(string title, QuestState newState) {
+
+	void OnQuestStateChanged (string title, QuestState newState) {
 
 		// make a quest marker on the tent
 		if (newState == QuestState.Active) {
 
 			GameObject collider = GameObject.Find ("FindYourTentQuestCollider");
 
-			GameObject questMarker = Instantiate (questMarkerPrefab, this.transform);
+			GameObject questMarker = Instantiate (questMarkerPrefab, this.transform) as GameObject;
+
 			questMarker.name = "QM_FindYourTentQuestCollider";
+
+			activeMarkers.Add (title, questMarker);
+		} else {
+			// inactive quests don't get marker
+			GameObject marker = activeMarkers[title] as GameObject;
+			if (marker != null) {
+				activeMarkers.Remove(title);
+				Destroy(marker);
+			}
 		}
 	}
 }
