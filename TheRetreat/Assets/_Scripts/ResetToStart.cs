d4de1d49f1b095c2d5f88c8ea6652343d1965fea
Luke Petruzzi <lukepetruzzi@gmail.com>
2018-05-09 16:12:28 -0400

    new build and reset script works with quest manager

---
+++ TheRetreat/Assets/_Scripts/ResetToStart.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ResetToStart : MonoBehaviour {
+	public static ResetToStart instance;
+	public bool showCursor = true;
+	// Use this for initialization
+	void Start () {
+		if(instance == null){
+			instance = this;
+			DontDestroyOnLoad(gameObject);
+		} else {
+			Destroy(gameObject);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+//		Debug.Log("Cursor.visible: " + Cursor.visible);
+		if(Input.GetKeyDown(KeyCode.R)){
+			Destroy (GameObject.Find ("Dialogue Manager"));
+
+			SceneManager.LoadScene(0);  
+			//          Destroy(GameObject.Find("Dialogue Manager"));
+			Invoke("ToggleCursor", 0.25f);
+			//          ToggleCursor();
+		}
+	}
+	void ToggleCursor(){
+		Cursor.visible = showCursor;
+//		Debug.Log("Cursor.visible: " + Cursor.visible);
+//		Debug.Log(Cursor.lockState = CursorLockMode.None);
+	}
+}
